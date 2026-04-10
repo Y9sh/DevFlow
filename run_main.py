8aa0cfcd257792bd221fb798ba22714266084b56
Y9sh <blaze125zr@gmail.com>
2026-04-11 00:18:49 +0800

    change main run_main

---
+++ run_main.py
@@ -0,0 +1,29 @@
+import os
+from git_manager import GIT
+git = GIT()
+def main() -> None:
+    # maybe i can make 2 modes, 1st one to use basic git and 2nd is automate fully git and run aider
+    # for now only 2nd mode 
+    # path for new project wihout any git
+    if not os.path.exists(".gitignore"):
+        git.create_gitignore()
+        git.add_more_git_ignore()
+        git.git_init()
+        while True:
+            # ask for rerun aider
+            rerun = input("Run aider (y/n)?")
+            if not rerun:
+                print("Invalid")
+                continue
+            if rerun == "y":
+                git.loop_step()
+            else:
+                print("See yaa~~~")
+                break    
+    else:
+        # path project already have git 
+        git.updated_gitignore()
+
+
+if __name__ == "__main__":
+    main()
